7a11ecd483fb3ead54d699b859188a0465323223
kelly <kelly.n.waite@gmail.com>
2017-01-25 14:25:41 -0700

    Add Letters to pictures; Add styling for layout

--- src/Keys.js
+++ src/Keys.js
@@ -3,48 +3,53 @@ import React from 'react';
 import css from "./styles/Keys.css";
 
 
-let arr = [];
-const makeImgArr = () =>  {
-  for( let i = 0; i <= 9; i++) {
-    let img = "cat" + i;
-    console.log(img);
-    // let element =  `<img src={${img}} alt="${img}"/>`
-    // arr.push(element);
-    arr.push(img);
-  }
-}
-
-makeImgArr();
-
 
 class Keys extends React.Component {
   Details = [
     {
-      image: require("./images/cat.jpg")
+      image: require("./images/cat.jpg"),
+      letter: "A",
+      key: "65"
     },
     {
-      image: require("./images/cat1.jpg")
+      image: require("./images/cat1.jpg"),
+      letter: "S",
+      key: "83"
     },
     {
-      image: require("./images/cat2.jpg")
+      image: require("./images/cat2.jpg"),
+      letter: "D",
+      key: "68"
     },
     {
-      image: require("./images/cat3.jpg")
+      image: require("./images/cat3.jpg"),
+      letter: "F",
+      key: "70"
     },
     {
-      image: require("./images/cat4.jpg")
+      image: require("./images/cat4.jpg"),
+      letter: "G",
+      key: "71"
     },
     {
-      image: require("./images/cat5.jpg")
+      image: require("./images/cat5.jpg"),
+      letter: "H",
+      key: "72"
     },
     {
-      image: require("./images/cat6.jpg")
+      image: require("./images/cat6.jpg"),
+      letter: "J",
+      key: "74"
     },
     {
-      image: require("./images/cat7.jpg")
+      image: require("./images/cat7.jpg"),
+      letter: "K",
+      key: "75"
     },
     {
-      image: require("./images/cat8.jpg")
+      image: require("./images/cat8.jpg"),
+      letter: "L",
+      key: "76"
     }
   ]
 
@@ -53,9 +58,13 @@ class Keys extends React.Component {
 
     return (
       <div className="keys">
-        <p>Test</p>
         {this.Details.map((c, i) => {
-         return  <img className="key" src={c.image} alt={c} key={i}/>
+         return  (
+           <div className="keyDiv" data-key={c.key}>
+             <img className="keyImg"  src={c.image} alt={c} key={i}/>
+             <h1 className="keyLetter">{c.letter}</h1>
+           </div>
+           )
         }) }
       </div>
     )
